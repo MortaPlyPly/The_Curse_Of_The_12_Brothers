6f679b4694ff9c3f14912656974b495792591cc1
Dominik Lisovski <Dominik.Lisovski@gmail.com>
2015-10-21 21:45:43 +0300

    Adding project

---
+++ Assets/Scripts/Judejimai/JudejimasKalnas.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JudejimasKalnas : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Trigger.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trigger : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Dont hurt me onichan");
+        Destroy(gameObject);
+
+        /*Instantiate(explosion, transform.position, transform.rotation);
+        gameController.AddScore(scoreValue);
+        if (other.tag == "Player 1" || other.tag == "Player 2")
+        {
+            gameController.UpdateLifes(-1, other);
+            return;
+        }*/
+        Destroy(other.gameObject);
+    }
+}
